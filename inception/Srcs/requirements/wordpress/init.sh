6ee85c10b5c879790e795a34d153df95f393c9d7
tcoeffet <tcoeffet@tcoeffet.myguest.virtualbox.org>
2026-05-04 17:33:17 +0200

    before security audit

--- inception/Srcs/requirements/wordpress/init.sh
+++ inception/Srcs/requirements/wordpress/init.sh
@@ -3,22 +3,22 @@ set -e
 
 echo "Starting WordPress init..."
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "Waiting for MariaDB..."
-    until mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" --ssl=0 -e "SELECT 1" >/dev/null 2>&1; do
-        sleep 2
-    done
-        echo "mariaDB & wordpress ready"
 
-    cd /var/www/html/
+echo "Waiting for MariaDB..."
+until mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" --ssl=0 -e "SELECT 1" >/dev/null 2>&1; do
+    sleep 2
+done
+    echo "mariaDB & wordpress ready"
+
+cd /var/www/html/
+
+if [ ! -f wp-config.php ]; then
     cp wp-config-sample.php wp-config.php
     sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
     sed -i "s/username_here/$DB_USER/g" wp-config.php
     sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
     sed -i "s/localhost/$DB_HOST/g" wp-config.php
     #setting the wp-config file to use env variables
-
-    sed -i 's|listen = .*|listen = 0.0.0.0:9000|' /etc/php83/php-fpm.d/www.conf
 fi
 
 if ! wp core is-installed --path=/var/www/html; then
@@ -31,8 +31,11 @@ if ! wp core is-installed --path=/var/www/html; then
       --admin_user="$WP_ADMIN_USER" \
       --admin_password="$WP_ADMIN_PASSWORD" \
       --admin_email="$WP_ADMIN_EMAIL" \
+      --allow-root \
       --skip-email
 fi
+
+sed -i 's|listen = .*|listen = 0.0.0.0:9000|' /etc/php83/php-fpm.d/www.conf
 
 exec php-fpm83 -F
 
